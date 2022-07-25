fbb827969cecb90caa41927a5195e2175caf3f01
jieG <2712788035@qq.com>
2022-07-25 21:14:20 +0800

    五种状态andGET请求

--- server.js
+++ server.js
@@ -6,8 +6,13 @@ const app = express();
 app.use(express.static(__dirname + '/src'));
 
 app.get('/test_get', (request, response) => {
-    console.log('有人请求了服务器');
+    console.log('有人请求了服务器，携带的query参数是：' + request.query);
     response.send('HELLO AJAX');
+})
+
+app.get('/test_get2/:name/:age', (request, response) => {
+    console.log('有人请求了服务器，携带的params参数是：' + request.params);
+    response.send('HELLO AJAX TWO');
 })
 
 app.listen(8080, (err) => {
