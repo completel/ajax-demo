d39ab4a795e13c159446c084e51610d34ffb27de
jieG <2712788035@qq.com>
2022-07-31 12:57:17 +0800

    cors 解决跨域

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 // 使用中间件解析urlencoded编码形式的请求体参数
 app.use(express.urlencoded({ extended: true }));
 // 使用中间件解析json编码形式的请求体参数
 app.use(express.json());
+app.use(cors());
 
 // 暴露静态资源
 app.use(express.static(__dirname + '/src'));
@@ -23,6 +25,18 @@ app.get('/test_jsonp', (request, response) => {
         { id: '2', name: '张三', age: 19 },
     ];
     response.send(`${callback}(${JSON.stringify(person)})`);
+})
+
+app.get('/test_cors', (request, response) => {
+    // 响应头
+    // response.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+    // response.setHeader('Access-Control-Allow-Origin', '*');
+    // response.setHeader('Access-Control-Expose-Headers', '*');
+    const person = [
+        { id: '1', name: '桔梗', age: 18 },
+        { id: '2', name: '张三', age: 19 },
+    ];
+    response.send(person);
 })
 
 // 响应GET请求----可以接受params参数
@@ -49,6 +63,19 @@ app.post('/test_post', (request, response) => {
     console.log('有人请求了test_post了', request.body);
     response.send('HELLO POST');
 })
+
+app.put('/test_put', (request, response) => {
+    // response.setHeader('Access-Control-Allow-Origin', '*');
+    // response.setHeader('Access-Control-Expose-Headers', '*');
+    response.send('HELLO_TEST_PUT');
+})
+
+/* app.options('/test_put', (request, response) => {
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Expose-Headers', '*');
+    response.setHeader('Access-Control-Allow-Methods', '*');
+    response.send();
+}) */
 
 app.listen(8080, (err) => {
     if (!err) console.log('服务器启动成功啦~');
